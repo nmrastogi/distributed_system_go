b798003f28a249b21d22d6201f5301326d4dfebb
nmrastogi <nmrastogi@gmail.com>
2026-02-22 15:46:43 -0800

    implement Raft state machine and log entry structures

--- go-raft/raft/raft.go
+++ go-raft/raft/raft.go
@@ -1,1 +1,39 @@
 package raft
+
+import "sync"
+
+type State int
+
+const (
+	Follower State = iota
+	Candidate
+	Leader
+)
+
+type Raft struct {
+	mu    sync.Mutex
+	id    int
+	peers []string
+
+	state State
+
+	currentTerm int
+	votedfor    int
+	log         []LogEntry
+
+	commitIndex int
+	lastApplied int
+}
+
+type LogEntry struct {
+	Term     int
+	LeaderID int
+}
+
+func NewRaft(id int, peers []string) *Raft {
+	return &Raft{
+		id:    id,
+		peers: peers,
+		state: Follower,
+	}
+}
